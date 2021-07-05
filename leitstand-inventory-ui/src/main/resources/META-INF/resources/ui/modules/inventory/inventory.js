720f94fe74a9577342fb48d344e2a2b6b8322883
Martin Steger <martin@rtbrick.com>
2021-07-05 16:54:21 +0200

    Change time-series endpoint to /api/v1/element/{element}/metrics

--- leitstand-inventory-ui/src/main/resources/META-INF/resources/ui/modules/inventory/inventory.js
+++ leitstand-inventory-ui/src/main/resources/META-INF/resources/ui/modules/inventory/inventory.js
@@ -652,10 +652,10 @@ export class TimeSeries extends Resource {
     load(settings){
         const params = Object.assign(this._cfg,settings);
         if(params.metric_name){
-            return this.json("/api/v1/timeseries/{{element_role}}/{{element_name}}/{{metric_name}}",params)
+            return this.json("/api/v1/elements/{{element_name}}/metrics/{{metric_name}}",params)
                        .GET();
         }
-        return this.json("/api/v1/timeseries/{{element_role}}/{{element_name}}",params)
+        return this.json("/api/v1/elements/{{element_name}}",params)
                    .GET();
     }
     
